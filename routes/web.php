372bcda7daa55897a0d1ba92ae28db097d94f14a
Suraj Adhikari <sssadhikari222@gmail.com>
2023-05-02 10:28:17 +0545

    Web

--- routes/web.php
+++ routes/web.php
@@ -36,36 +36,47 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+#Admin Route
 Route::get('/admin',[AdminController::class,'showForm']);
 Route::post('/admin/add_company',[AdminController::class,'addcompany'])->name('admin.add_company');
+Route::get('/admin/company',[AdminController::class,'companymanage'])->name('admin.company');
+Route::get('/admin/view_company',[AdminController::class,'viewcompany'])->name('admin.view_company');
+Route::get('/admin/del_company/{id}',[AdminController::class,'delcompany'])->name('admin.del_company');
+Route::post('/admin/login',[AdminController::class,'checkLogin'])->name('admin.login');
+Route::get('/admin/logout',[AdminController::class,'logout'])->name('admin.logout');
+Route::get('/admin/admin_dash',[AdminController::class,'showAdmin']);
+
+
+
+#Company
 Route::post('/company/add_clinic',[CompanyController::class,'addclinic'])->name('company.add_clinic');
 Route::post('/company/add_user',[CompanyController::class,'adduser'])->name('company.add_user');
 Route::post('/company/add_token',[CompanyController::class,'addtoken'])->name('company.add_token');
-
-Route::get('/admin/company',[AdminController::class,'companymanage'])->name('admin.company');
 Route::get('/company/clinic',[CompanyController::class,'clinicmanage'])->name('company.clinic');
 Route::get('/company/user',[CompanyController::class,'usermanage'])->name('company.user');
 Route::get('/company/token',[CompanyController::class,'tokenmanage'])->name('company.token');
-Route::get('/admin/view_company',[AdminController::class,'viewcompany'])->name('admin.view_company');
 Route::get('/company/view_clinic',[CompanyController::class,'viewclinic'])->name('company.view_clinic');
 Route::get('/company/view_user',[CompanyController::class,'viewuser'])->name('company.view_user');
-Route::get('/admin/del_company/{id}',[AdminController::class,'delcompany'])->name('admin.del_company');
 Route::get('/company/del_token/{id}',[CompanyController::class,'deltoken'])->name('company.del_token');
 Route::get('/company/del_clinic/{id}',[CompanyController::class,'delclinic'])->name('company.del_clinic');
 Route::get('/company/del_user/{id}',[CompanyController::class,'deluser'])->name('company.del_user');
-Route::get('/users',[UserController::class,'showForm']);
-Route::get('/clinic',[ClinicController::class,'showForm']);
 Route::get('/company',[CompanyController::class,'showForm']);
-Route::post('/admin/login',[AdminController::class,'checkLogin'])->name('admin.login');
-
-Route::post('/users/login',[UserController::class,'checkLogin'])->name('users.login');
-Route::post('/clinic/login',[ClinicController::class,'checkLogin'])->name('clinic.login');
 Route::post('/company/login',[CompanyController::class,'checkLogin'])->name('company.login');
-Route::get('/admin/logout',[AdminController::class,'logout'])->name('admin.logout');
-Route::get('/users/logout',[UserController::class,'logout'])->name('users.logout');
-Route::get('/clinic/logout',[ClinicController::class,'logout'])->name('clinic.logout');
 Route::get('/company/logout',[CompanyController::class,'logout'])->name('company.logout');
-Route::get('/admin/admin_dash',[AdminController::class,'showAdmin']);
+
+
+#Users
+Route::get('/users',[UserController::class,'showForm']);
+Route::get('/users/logout',[UserController::class,'logout'])->name('users.logout');
+Route::post('/users/login',[UserController::class,'checkLogin'])->name('users.login');
+
+
+#Clinic
+Route::post('/clinic/login',[ClinicController::class,'checkLogin'])->name('clinic.login');
+Route::get('/clinic',[ClinicController::class,'showForm']);
+Route::get('/clinic/logout',[ClinicController::class,'logout'])->name('clinic.logout');
+
+
 
 /*
 Route::middleware(['admin'])->group(function () {
